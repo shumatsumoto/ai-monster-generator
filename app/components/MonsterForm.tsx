07862c7a1c434eeb5238406f14295b48c62afd5f
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-01-02 00:35:38 +0900

    settings

--- app/components/MonsterForm.tsx
+++ app/components/MonsterForm.tsx
@@ -1,34 +1,52 @@
-import React, {useState} from 'react';
-import styles from "./MonsterForm.module.css";
-import {MONSTER_ATTRIBUTES} from "@/app/constants/attributes";
+import React, { useState } from 'react'
+import styles from './MonsterForm.module.css'
+import { MONSTER_ATTRIBUTES } from '@/app/constants/attributes'
 
 type MonsterFormProps = {
-  onSubmit: (description: string, attribute: string) => void;
+  onSubmit: (description: string, attribute: string) => void
 }
 
-const MonsterForm = ({onSubmit}: MonsterFormProps) => {
-  const [description, setDescription] = useState("");
-  const [attribute, setAttribute] = useState("");
+const MonsterForm = ({ onSubmit }: MonsterFormProps) => {
+  const [description, setDescription] = useState('')
+  const [attribute, setAttribute] = useState('')
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSubmit(description, attribute);
+    e.preventDefault()
+    onSubmit(description, attribute)
   }
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <div className={styles.formGroup}>
-        <label htmlFor="description" className={styles.label}>dscription</label>
-        <input type="text" id="description" value={description} onChange={e => setDescription(e.target.value)} placeholder="example:cute" required className={styles.input} />
+        <label htmlFor="description" className={styles.label}>
+          dscription
+        </label>
+        <input
+          type="text"
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="example:cute"
+          required
+          className={styles.input}
+        />
       </div>
 
       <div className={styles.formGroup}>
         <label htmlFor="attribute" className={styles.label}>
           モンスターの属性を選択
         </label>
-        <select id="attribute" value={attribute} onChange={e => setAttribute(e.target.value)} required className={styles.select}>
+        <select
+          id="attribute"
+          value={attribute}
+          onChange={(e) => setAttribute(e.target.value)}
+          required
+          className={styles.select}
+        >
           <option value="">選択してください</option>
           {MONSTER_ATTRIBUTES.map((attribute) => (
-            <option key={attribute.en} value={attribute.en}>{attribute.ja}</option>
+            <option key={attribute.en} value={attribute.en}>
+              {attribute.ja}
+            </option>
           ))}
         </select>
       </div>
@@ -37,7 +55,7 @@ const MonsterForm = ({onSubmit}: MonsterFormProps) => {
         モンスターの画像生成
       </button>
     </form>
-  );
+  )
 }
 
-export default MonsterForm;
+export default MonsterForm
